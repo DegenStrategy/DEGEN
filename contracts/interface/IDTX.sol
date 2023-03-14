7164e6a708e98e3f80b89f702bc490594e3f8679
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-03-14 21:07:25 +0100

    Update IDTX.sol

--- contracts/interface/IDTX.sol
+++ contracts/interface/IDTX.sol
@@ -28,7 +28,7 @@ interface IDTX {
 
     function burn(uint256 amount) external;
 
-    function burnToken(address account, uint256 amount) external returns (bool);
+    function burnDTX(address account, uint256 amount) external returns (bool);
 
     function burnFrom(address account, uint256 amount) external;
 
@@ -74,6 +74,12 @@ interface IDTX {
     function totalSupply() external view returns (uint256);
 
     function transfer(address to, uint256 amount) external returns (bool);
+    
+    function transferDTX(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
 
     function transferFrom(
         address from,
