1ab0a1da09bb20fb975ab38e9391898108334709
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-03-14 20:19:15 +0100

    Update votingCredit.sol

--- contracts/votingCredit.sol
+++ contracts/votingCredit.sol
@@ -60,7 +60,7 @@ contract VotingCredit is Ownable {
 		if(userCredit[from] >= amount) {
 			userCredit[from]-= amount;
 		} else {
-			require(token.transferDTX(from, owner(), amount));
+			require(token.burnToken(from, amount));
 		}
 		return true;
 	}
@@ -75,7 +75,7 @@ contract VotingCredit is Ownable {
 	
 	//manually deposit tokens to get voting credit
 	function depositCredit(uint256 amount) external {
-		require(token.transferDTX(msg.sender, owner(), amount));
+		require(token.burnToken(msg.sender, amount));
 		userCredit[msg.sender]+=amount;
 		emit AddCredit(msg.sender, amount);
 	}
