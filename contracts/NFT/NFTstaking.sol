8ff5a23583dc9f1897e663853af33fa0defb345d
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-03-14 23:19:49 +0100

    Update NFTstaking.sol

--- contracts/NFT/NFTstaking.sol
+++ contracts/NFT/NFTstaking.sol
@@ -21,10 +21,10 @@ interface INFTallocation {
 }
 
 /**
- * DTX NFT staking contract
- * !!! Warning: !!! Licensed under Business Source License 1.1 (BSL 1.1)
+ * XPD NFT staking contract
+ * !!! Warning: !!! Copyrighted
  */
-contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
+contract XPDnftStaking is ReentrancyGuard, ERC721Holder {
     using SafeERC20 for IERC20;
 
     struct UserInfo {
@@ -44,29 +44,23 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
         uint256 minServe;
     }
 
-    IERC20 public immutable token; // DTX token
+    IERC20 public immutable token = IERC20(); // XPD token
 	
-    IERC20 public immutable dummyToken; 
+    IERC20 public immutable dummyToken = IERC20(); 
 
-    IMasterChef public masterchef;  
+    IMasterChef public masterchef = IMasterChef();  
 
     mapping(address => UserInfo[]) public userInfo;
-    mapping(address => UserSettings) public userSettings; 
     mapping(address => PoolPayout) public poolPayout; //determines the percentage received depending on withdrawal option
  
-	uint256 public poolID; 
+	uint256 public poolID = ; 
     uint256 public totalAllocation = 10000;
     uint256 public accDtxPerShare;
     address public admin; //admin = governing contract!
     address public treasury; //penalties
-    address public allocationContract; // PROXY CONTRACT for looking up allocations
+    address public allocationContract = ; // PROXY CONTRACT for looking up allocations
 
     uint256 public tokenDebt; //sum of allocations of all deposited NFTs
-
-    //if user settings not set, use default
-    address public defaultHarvest; //pool address to harvest into
-    uint256 public defaultHarvestThreshold = 1000000;
-    uint256 public defaultFeeToPay = 250; //fee for calling 2.5% default
 
     uint256 public defaultDirectPayout = 500; //5% if withdrawn into wallet
 
@@ -76,39 +70,25 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
 
     event AddVotingCredit(address indexed user, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 _stakeID, address indexed token, uint256 tokenID);
-    event Harvest(address indexed harvester, address indexed benficiary, address harvestInto, uint256 harvestAmount, uint256 penalty, uint256 callFee); //harvestAmount contains the callFee
     event SelfHarvest(address indexed user, address harvestInto, uint256 harvestAmount, uint256 penalty);
 
     /**
      * @notice Constructor
-     * @param _token: DTX token contract
+     * @param _token: XPD token contract
      * @param _dummyToken: Dummy token contract
      * @param _masterchef: MasterChef contract
      * @param _admin: address of the admin
      * @param _treasury: address of the treasury (collects fees)
      */
     constructor(
-        IERC20 _token,
-        IERC20 _dummyToken,
-        IMasterChef _masterchef,
         address _admin,
-        address _treasury,
-        uint256 _poolID,
-        address _allocationContract,
-		address _longestPool
+        address _treasury
     ) {
-        token = _token;
-        dummyToken = _dummyToken;
-        masterchef = _masterchef;
         admin = _admin;
         treasury = _treasury;
-        poolID = _poolID;
-        allocationContract = _allocationContract;
-		defaultHarvest = _longestPool;
 
 		
         IERC20(_dummyToken).safeApprove(address(_masterchef), type(uint256).max);
-		set pool payouts at launch
 		/*
 		poolPayout[].amount = 750;
         poolPayout[].minServe = 864000;
@@ -218,15 +198,6 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
         IERC721(_tokenAddress).safeTransferFrom(address(this), msg.sender, _tokenID); //withdraw NFT
     }  
 
-    function setUserSettings(uint256 _harvestThreshold, uint256 _feeToPay, address _harvestInto) external {
-        require(_feeToPay <= 250, "max 2.5%");
-        if(_harvestInto != msg.sender) { require(poolPayout[_harvestInto].amount != 0, "incorrect pool!"); }
-        UserSettings storage _setting = userSettings[msg.sender];
-        _setting.harvestThreshold = _harvestThreshold;
-        _setting.feeToPay = _feeToPay;
-        _setting.pool = _harvestInto; //default pool to harvest into(or payout directly)
-        emit UserSettingUpdate(msg.sender, _harvestInto, _harvestThreshold, _feeToPay);
-    }
 
     //harvest own earnings
     function selfHarvest(address _harvestInto) external nonReentrant {
@@ -258,107 +229,38 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
     }
 
 
-    //harvest earnings of another user(receive fees)
-    function proxyHarvest(address _beneficiary) external {
-        UserInfo[] storage user = userInfo[_beneficiary];
+    function selfHarvestCustom(address _harvestInto, uint256[] memory _stakeID) external nonReentrant {
+        UserInfo[] storage user = userInfo[msg.sender];
 		require(user.length > 0, "user has no stakes");
         harvest();
         uint256 _totalWithdraw = 0;
         uint256 _toWithdraw = 0;
         uint256 _payout = 0;
-
-        UserSettings storage _userSetting = userSettings[_beneficiary];
-
-        address _harvestInto = _userSetting.pool;
-        uint256 _minThreshold = _userSetting.harvestThreshold;
-        uint256 _callFee = _userSetting.feeToPay;
-
-        if(_minThreshold == 0) { _minThreshold = defaultHarvestThreshold; }
-        if(_callFee == 0) { _callFee = defaultFeeToPay; }
-
-        for(uint256 i = 0; i<user.length; i++) {
-            _toWithdraw = user[i].allocation * accDtxPerShare / 1e12 - user[i].debt;
-            user[i].debt = user[i].allocation * accDtxPerShare / 1e12;
-            _totalWithdraw+= _toWithdraw;
-        }
-
-        if(_harvestInto == _beneficiary) {
-            //fee paid to harvester
-            _payout = _toWithdraw * defaultDirectPayout / 10000;
-            _callFee = _payout * _callFee / 10000;
-            token.safeTransfer(msg.sender, _callFee); 
-            token.safeTransfer(_beneficiary, (_payout - _callFee)); 
-        } else {
-            if(_harvestInto == address(0)) {
-                _harvestInto = defaultHarvest; //default pool
-            } //harvest Into is correct(checks if valid when user initiates the setting)
-            _payout = _toWithdraw * poolPayout[_harvestInto].amount / 10000;
-            require(_payout > _minThreshold, "minimum threshold not met");
-            _callFee = _payout * _callFee / 10000;
-            token.safeTransfer(msg.sender, _callFee); 
-            IacPool(_harvestInto).giftDeposit((_payout - _callFee), _beneficiary, poolPayout[_harvestInto].minServe);
-        }
-        uint256 _penalty = _toWithdraw - _payout;
-        token.safeTransfer(treasury, _penalty); //penalty to treasury
-
-        emit Harvest(msg.sender, _beneficiary, _harvestInto, _payout, _penalty, _callFee);
-    }
-
-	//copy+paste of the previous function, can harvest custom stake ID
-	//In case user has too many stakes, or if some are not worth harvesting
-	function proxyHarvestCustom(address _beneficiary, uint256[] calldata _stakeID) public {
-        require(_stakeID.length <= userInfo[_beneficiary].length, "incorrect Stake list");
-        UserInfo[] storage user = userInfo[_beneficiary];
-        harvest();
-        uint256 _totalWithdraw = 0;
-        uint256 _toWithdraw = 0;
-        uint256 _payout = 0;
-
-        UserSettings storage _userSetting = userSettings[_beneficiary];
-
-        address _harvestInto = _userSetting.pool;
-        uint256 _minThreshold = _userSetting.harvestThreshold;
-        uint256 _callFee = _userSetting.feeToPay;
-
-        if(_minThreshold == 0) { _minThreshold = defaultHarvestThreshold; }
-        if(_callFee == 0) { _callFee = defaultFeeToPay; }
-
+ 
         for(uint256 i = 0; i<_stakeID.length; i++) {
             _toWithdraw = user[_stakeID[i]].allocation * accDtxPerShare / 1e12 - user[_stakeID[i]].debt;
             user[_stakeID[i]].debt = user[_stakeID[i]].allocation * accDtxPerShare / 1e12;
             _totalWithdraw+= _toWithdraw;
         }
 
-        if(_harvestInto == _beneficiary) {
-            _payout = _toWithdraw * defaultDirectPayout / 10000;
-            _callFee = _payout * _callFee / 10000;
-            token.safeTransfer(msg.sender, _callFee); 
-            token.safeTransfer(_beneficiary, (_payout - _callFee)); 
+        if(_harvestInto == msg.sender) {
+            _payout = _totalWithdraw * defaultDirectPayout / 10000;
+            token.safeTransfer(msg.sender, _payout); 
         } else {
-            if(_harvestInto == address(0)) {
-                _harvestInto = defaultHarvest; //default pool
-            } 
-            _payout = _toWithdraw * poolPayout[_harvestInto].amount / 10000;
-            require(_payout > _minThreshold, "minimum threshold not met");
-            _callFee = _payout * _callFee / 10000;
-            token.safeTransfer(msg.sender, _callFee); 
-            IacPool(_harvestInto).giftDeposit((_payout - _callFee), _beneficiary, poolPayout[_harvestInto].minServe);
+            require(poolPayout[_harvestInto].amount != 0, "incorrect pool!");
+            _payout = _totalWithdraw * poolPayout[_harvestInto].amount / 10000;
+            IacPool(_harvestInto).giftDeposit(_payout, msg.sender, poolPayout[_harvestInto].minServe);
         }
-        uint256 _penalty = _toWithdraw - _payout;
+        uint256 _penalty = _totalWithdraw - _payout;
         token.safeTransfer(treasury, _penalty); //penalty to treasury
-        
-        emit Harvest(msg.sender, _beneficiary, _harvestInto, _payout, _penalty, _callFee);
+
+        emit SelfHarvest(msg.sender, _harvestInto, _payout, _penalty);
     }
 	
-	function massHarvest(address[] calldata beneficiary, uint256[][] calldata stakeID) external {
-        for(uint256 i=0; i<beneficiary.length; i++) {
-            proxyHarvestCustom(beneficiary[i], stakeID[i]);
-        }
-    }
 	
     function viewStakeEarnings(address _user, uint256 _stakeID) external view returns (uint256) {
 		UserInfo storage _stake = userInfo[_user][_stakeID];
-        uint256 _pending = _stake.allocation * virtualAccDtxPerShare() / 1e12 - _stake.debt;
+        uint256 _pending = _stake.allocation * virtualaccDtxPerShare() / 1e12 - _stake.debt;
         return _pending;
     }
 
@@ -369,7 +271,7 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
 		uint256 _totalPending = 0;
 		
 		for(uint256 i=0; i < nrOfUserStakes; i++) {
-			_totalPending+= _stake[i].allocation * virtualAccDtxPerShare() / 1e12 - _stake[i].debt;
+			_totalPending+= _stake[i].allocation * virtualaccDtxPerShare() / 1e12 - _stake[i].debt;
 		}
 		
 		return _totalPending;
@@ -377,7 +279,7 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
 	//we want user deposit, we want total deposited, we want pending rewards, 
 	function multiCall(address _user, uint256 _stakeID) external view returns(uint256, uint256, uint256, address, uint256) {
 		UserInfo storage user = userInfo[_user][_stakeID];
-		uint256 _pending = user.allocation * virtualAccDtxPerShare() / 1e12 - user.debt;
+		uint256 _pending = user.allocation * virtualaccDtxPerShare() / 1e12 - user.debt;
 		return(user.allocation, totalAllocation, _pending, user.tokenAddress, user.tokenID);
 	}
 	
@@ -397,7 +299,7 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
 		} else {
 			_alloc = INFTallocation(allocationContract).getAllocation(user.tokenAddress, user.tokenID, user.allocContract);
 		}
-        if(_alloc == 0) { //no longer valid, anyone can push out and withdraw NFT to the owner (copy+paste withdraw option) 
+        if(_alloc == 0) { //no longer valid, anyone can push out and withdraw NFT to the owner (copy+paste withdraw option). If owner doesn't take action, rewards are lost
             uint256 currentAmount = user.allocation * accDtxPerShare / 1e12 - user.debt;
             totalAllocation-= user.allocation;
 
@@ -408,21 +310,6 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
             
             _removeStake(_staker, _stakeID); //delete the stake
 
-            address _harvestInto = userSettings[_staker].pool;
-            if(_harvestInto == address(0)) { _harvestInto = defaultHarvest; } 
-
-            uint256 _toWithdraw;      
-            if(_harvestInto == _staker) { 
-                _toWithdraw = currentAmount * defaultDirectPayout / 10000;
-                currentAmount = currentAmount - _toWithdraw;
-                token.safeTransfer(_staker, _toWithdraw);
-            } else {
-                _toWithdraw = currentAmount * poolPayout[_harvestInto].amount / 10000;
-				if(_toWithdraw > IacPool(_harvestInto).minimumGift()) {
-					currentAmount = currentAmount - _toWithdraw;
-					IacPool(_harvestInto).giftDeposit(_toWithdraw, _staker, poolPayout[_harvestInto].minServe);
-				}
-            }
             token.safeTransfer(treasury, currentAmount); //penalty goes to governing contract
 
             IERC721(_tokenAddress).safeTransferFrom(address(this), _staker, _tokenID); //withdraw NFT
@@ -435,7 +322,7 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
     }
 	
 	// Adding virtual harvest for the external viewing
-	function virtualAccDtxPerShare() public view returns (uint256) {
+	function virtualaccDtxPerShare() public view returns (uint256) {
 		uint256 _pending = IMasterChef(masterchef).pendingDtx(poolID, address(this));
 		return (accDtxPerShare + _pending * 1e12  / totalAllocation);
 	}
@@ -468,26 +355,13 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
 	//allocation contract contains the decentralized proccess for updating setting, but so does the admin(governor)
     function setPoolPayout(address _poolAddress, uint256 _amount, uint256 _minServe) external {
         require(msg.sender == allocationContract || msg.sender == admin, "must be set by allocation contract or admin");
-		if(_poolAddress == address(0)) {
-			require(_amount <= 10000, "out of range");
-			defaultDirectPayout = _amount;
-		} else if (_poolAddress == address(1)) {
-			defaultHarvestThreshold = _amount;
-		} else if (_poolAddress == address(2)) {
-			require(_amount <= 1000, "out of range"); //max 10%
-			defaultFeeToPay = _amount;
-		} else {
-			require(_amount <= 10000, "out of range"); 
-			poolPayout[_poolAddress].amount = _amount;
-        	poolPayout[_poolAddress].minServe = _minServe; //mandatory lockup(else stake for 5yr, withdraw with 82% penalty and receive 18%)
-		}
+		require(_amount <= 10000, "out of range"); 
+		poolPayout[_poolAddress].amount = _amount;
+		poolPayout[_poolAddress].minServe = _minServe; //mandatory lockup(else stake for 5yr, withdraw with 82% penalty and receive 18%)
     }
     
-    function updateSettings(address _defaultHarvest, uint256 _threshold, uint256 _defaultFee, uint256 _defaultDirectHarvest) external adminOnly {
-        defaultHarvest = _defaultHarvest; //longest pool should be the default
-        defaultHarvestThreshold = _threshold;
-        defaultFeeToPay = _defaultFee;
-        defaultDirectPayout = _defaultDirectHarvest;
+    function updateSettings(uint256 _defaultDirectPayout) external adminOnly {
+        defaultDirectPayout = _defaultDirectPayout;
     }
 
     /**
@@ -499,24 +373,17 @@ contract DTXnftStaking is ReentrancyGuard, ERC721Holder {
 	
 
     /**
-     * @return Returns total pending DTX rewards
+     * @return Returns total pending XPD rewards
      */
-    function calculateTotalPendingDTXRewards() external view returns (uint256) {
+    function calculateTotalpendingDtxRewards() external view returns (uint256) {
         return(IMasterChef(masterchef).pendingDtx(poolID, address(this)));
     }
 
 	
-    /**
-     * calculates pending rewards + balance of tokens in this address + artificial token debt(how much each NFT is worth)
-	 * we harvest before every action, pending rewards not needed
-     */
-    function balanceOf() internal view returns (uint256) {
-        return token.balanceOf(address(this)) + tokenDebt; 
-    }
 	//public lookup for UI
     function publicBalanceOf() public view returns (uint256) {
         uint256 amount = IMasterChef(masterchef).pendingDtx(poolID, address(this)); 
-        return token.balanceOf(address(this)) + amount + tokenDebt; 
+        return token.balanceOf(address(this)) + amount; 
     }
 	
 	/*
