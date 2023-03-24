6d3c9c30295651ab10f1057791569056698c309b
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-03-24 21:58:29 +0100

    Update XPD.sol

--- contracts/XPD.sol
+++ contracts/XPD.sol
@@ -13,18 +13,6 @@ import "./interface/IGovernor.sol";
 contract XPD is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
 	string private _name;
     string private _symbol;
-	
-	bool public allowTrustedContracts = true;
-	
-    //trusted contracts can transfer and burn without allowance
-	//only governor can set trusted contracts and governor is decentralized(you are the governor)
-	mapping(address => bool) public trustedContract;
-	
-	//additionally, users can disable the feature and revert to mandatory allowance(as per ERC20 standard)
-	mapping(address => bool) public requireAllowance;
-	
-	//makes it easier to verify(without event logs)
-	uint256 public trustedContractCount; 
     
 	constructor() ERC20("PulseDAO", "XPD") {
 		_name = string("PulseDAO");
@@ -36,64 +24,16 @@ contract XPD is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
     	_;
     }
 	
-	event TrustedContract(address contractAddress, bool setting);
-	event RequireAllowance(address wallet, bool setting);
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 	
-	
-	/*
-	* allows burn without allowance for trusted contracts
-	* trusted contracts can only be set through governor and governor is decentralized
-	* trusted contract shall always require the signature of the owner(from address shall only ever be msg.sender)
-	* governor also owns Masterchef which can set emissions and create new pools(mint new tokens)
-	* Assuming governor is decentralized and secure, it improves user experience without compromising on security
-	*/
-    function burnToken(address account, uint256 amount) public returns (bool) {
-        address spender = _msgSender();
-		if(!requireAllowance[account] && trustedContract[spender]) {
-			_burn(account, amount);
-		} else {
-			_spendAllowance(account, spender, amount);
-        	_burn(account, amount);
-		}
-		return true;
+    function burnToken(address account, uint256 amount) external onlyOwner returns (bool) {
+	_burn(account, amount);
+	return true;
     }
-	
-	//only owner can set trusted Contracts
-	function setTrustedContract(address _contractAddress, bool _setting) external decentralizedVoting {
-		if(allowTrustedContracts && trustedContract[_contractAddress] != _setting) { //prevents messing up the count. Using if to avoid revert
-			trustedContract[_contractAddress] = _setting;
-			_setting ? trustedContractCount++ : trustedContractCount--;
-			emit TrustedContract(_contractAddress, _setting);
-		}
-	}
-	
-	// in case there is a contract for a specific task, it can renounce it's trusted status upon completion
-	// note that trusted contracts can only be set through the consensus. Contracts can't magically become trusted out of nowhere
-	function selfRenounce() external {
-		if(trustedContract[msg.sender]) {
-			trustedContract[msg.sender] = false;
-			trustedContractCount--;
-			emit TrustedContract(msg.sender, false);
-		}
-	}
-	
-	//option to globally disable trusted contracts and revert to the ERC20 standard
-	//first set all current trustedContract settings to false, then call this function to renounce
-	function renounceTrustedContracts() external decentralizedVoting {
-		require(allowTrustedContracts, "can only be turned off once and made immutable");
-		allowTrustedContracts = false;
-	}
-	
-	//Option for individual addresses to revert to the ERC20 standard and require allowance for transferFrom(for exchanges)
-	function requireAllowanceForTransfer(bool _setting) external {
-		requireAllowance[msg.sender] = _setting;
-		
-		emit RequireAllowance(msg.sender, _setting);
-	}
+
 	
 	//Standard ERC20 makes name and symbol immutable
 	//We add potential to rebrand for full flexibility if stakers choose to do so through voting
