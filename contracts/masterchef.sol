6476a89e3c92ad4556a724e801bea5b3f1220789
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-03-31 23:04:43 +0200

    Update masterchef.sol
    Staking contracts become the sole participants for pool ID.
    Must remove dummy contracts everywhere, no longer needed

--- contracts/masterchef.sol
+++ contracts/masterchef.sol
@@ -12,31 +12,12 @@ import "./interface/IDTX.sol";
 contract DTXChef is Ownable, ReentrancyGuard {
 	using SafeMath for uint256;
     using SafeERC20 for IERC20;
-	
-    // Info of each user.
-    struct UserInfo {
-        uint256 amount;         // How many LP tokens the user has provided.
-        uint256 rewardDebt;     // Reward debt. See explanation below.
-        //
-        // We do some fancy math here. Basically, any point in time, the amount of DTX
-        // entitled to a user but is pending to be distributed is:
-        //
-        //   pending reward = (user.amount * pool.accDtxPerShare) - user.rewardDebt
-        //
-        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accDtxPerShare` (and `lastRewardBlock`) gets updated.
-        //   2. User receives the pending reward sent to his/her address.
-        //   3. User's `amount` gets updated.
-        //   4. User's `rewardDebt` gets updated.
-    }
 
     // Info of each pool.
-    struct PoolInfo {
-        IERC20 lpToken;           // Address of LP token contract.
+    struct PoolInfo {       // Address of LP token contract.
         uint256 allocPoint;       // How many allocation points assigned to this pool. DTXs to distribute per block.
         uint256 lastRewardBlock;  // Last block number that DTXs distribution occurs.
-        uint256 accDtxPerShare;   // Accumulated DTX per share, times 1e12. See below.
-        uint16 depositFeeBP;      // Deposit fee in basis points
+        address participant;   // participating contract    
     }
 
     // The DTX TOKEN!
@@ -52,12 +33,10 @@ contract DTXChef is Ownable, ReentrancyGuard {
 
     // Info of each pool.
     PoolInfo[] public poolInfo;
-    // Info of each user that stakes LP tokens.
-    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
 	// User Credit (can publish such amount of tokens)
 	mapping(address => uint256) public credit;
 	// Does Pool Already Exist?
-	mapping(IERC20 => bool) public poolExistence;
+	mapping(address => bool) public existingParticipant;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
     // The block number when DTX mining starts.
@@ -68,7 +47,6 @@ contract DTXChef is Ownable, ReentrancyGuard {
 	//makes it easier to verify(without event logs)
 	uint256 public trustedContractCount; 
 
-    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
 	event UpdateEmissions(address indexed user, uint256 newEmissions);
@@ -106,52 +84,41 @@ contract DTXChef is Ownable, ReentrancyGuard {
 	}
 	
     // Add a new lp to the pool. Can only be called by the owner.
-    function add(uint256 _allocPoint, IERC20 _lpToken, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
-        require(_depositFeeBP <= 10000, "add: invalid deposit fee basis points");
-		//prevents adding a contract that is not a token/LPtoken(incompatitible)
-		require(_lpToken.balanceOf(address(this)) >= 0, "incompatitible token contract");
-		//prevents same LP token from being added twice
-		require(!poolExistence[_lpToken], "LP pool already exists");
+    function add(uint256 _allocPoint, address _participant, bool _withUpdate) public onlyOwner {
+		require(!existingParticipant[_participant], "contract already participating");
 		
         if (_withUpdate) {
             massUpdatePools();
         }
         uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
         totalAllocPoint = totalAllocPoint.add(_allocPoint);
-		poolExistence[_lpToken] = true;
+		existingParticipant[_participant] = true;
         poolInfo.push(PoolInfo({
-            lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accDtxPerShare: 0,
-            depositFeeBP: _depositFeeBP
+            participant: _participant
         }));
     }
 
     // Update the given pool's DTX allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
-        require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         if (_withUpdate) {
             massUpdatePools();
         }
         totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
         poolInfo[_pid].allocPoint = _allocPoint;
-        poolInfo[_pid].depositFeeBP = _depositFeeBP;
     }
 
 
     // View function to see pending DTXs on frontend.
     function pendingDtx(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][_user];
-        uint256 accDtxPerShare = pool.accDtxPerShare;
-        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
-        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+        if (block.number > pool.lastRewardBlock && pool.participant != address(0)) {
             uint256 multiplier = block.number.sub(pool.lastRewardBlock);
             uint256 dtxReward = multiplier.mul(DTXPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accDtxPerShare = accDtxPerShare.add(dtxReward).mul(1e12).div(lpSupply);
+            return dtxReward;
         }
-        return user.amount.mul(accDtxPerShare).div(1e12).sub(user.rewardDebt);
+        return 0;
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -168,59 +135,35 @@ contract DTXChef is Ownable, ReentrancyGuard {
         if (block.number <= pool.lastRewardBlock) {
             return;
         }
-        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
-        if (lpSupply == 0 || pool.allocPoint == 0) {
+        if (pool.participant == address(0) || pool.allocPoint == 0) {
             pool.lastRewardBlock = block.number;
             return;
         }
         uint256 multiplier = block.number.sub(pool.lastRewardBlock);
         uint256 dtxReward = multiplier.mul(DTXPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
         dtx.mint(devaddr, dtxReward.mul(governorFee).div(10000));
-        pool.accDtxPerShare = pool.accDtxPerShare.add(dtxReward).mul(1e12).div(lpSupply);
+		credit[pool.participant] = credit[pool.participant] + dtxReward;
         pool.lastRewardBlock = block.number;
-    }
-
-    // Deposit LP tokens to MasterChef for DTX allocation.
-    function deposit(uint256 _pid, uint256 _amount) public nonReentrant {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        updatePool(_pid);
-        if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accDtxPerShare).div(1e12).sub(user.rewardDebt);
-            if(pending > 0) {
-				credit[msg.sender]+= pending;
-            }
-        }
-        if(_amount > 0) {
-            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
-            if(pool.depositFeeBP > 0){
-                uint256 depositFee = _amount.mul(pool.depositFeeBP).div(10000);
-                pool.lpToken.safeTransfer(feeAddress, depositFee);
-                user.amount = user.amount.add(_amount).sub(depositFee);
-            }else{
-                user.amount = user.amount.add(_amount);
-            }
-        }
-        user.rewardDebt = user.amount.mul(pool.accDtxPerShare).div(1e12);
-        emit Deposit(msg.sender, _pid, _amount);
     }
 
     // Withdraw LP tokens from MasterChef.
     function withdraw(uint256 _pid, uint256 _amount) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        require(user.amount >= _amount, "withdraw: not good");
+        require(pool.participant == msg.sender, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accDtxPerShare).div(1e12).sub(user.rewardDebt);
-        if(pending > 0) {
-			credit[msg.sender]+= pending;
-        }
         if(_amount > 0) {
-            user.amount = user.amount.sub(_amount);
-            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+            pool.participant = address(0);
+            poolInfo[_pid].allocPoint = 0;
+            totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint);
         }
-        user.rewardDebt = user.amount.mul(pool.accDtxPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    function stopPublishing(uint256 _pid) external onlyOwner {
+        updatePool(_pid);
+        pool.participant = address(0);
+        poolInfo[_pid].allocPoint = 0;
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint);
     }
 	
 	// In case pools are changed (on migration old contract transfers it's credit to the new one)
@@ -239,17 +182,6 @@ contract DTXChef is Ownable, ReentrancyGuard {
 			emit TrustedContract(_contractAddress, _setting);
 		}
 	}
-
-    // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) public nonReentrant {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        uint256 amount = user.amount;
-        user.amount = 0;
-        user.rewardDebt = 0;
-        pool.lpToken.safeTransfer(address(msg.sender), amount);
-        emit EmergencyWithdraw(msg.sender, _pid, amount);
-    }
 
 	function setGovernorFee(uint256 _amount) public onlyOwner {
 		require(_amount <= 1000 && _amount >= 0);
