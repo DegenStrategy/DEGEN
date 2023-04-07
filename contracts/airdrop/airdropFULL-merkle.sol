bddb4bf0fad1a041fcbea7b4559ab96643f98374
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-04-07 19:40:37 +0200

    Update airdropFULL-merkle.sol

--- contracts/airdrop/airdropFULL-merkle.sol
+++ contracts/airdrop/airdropFULL-merkle.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../interface/IDTX.sol";
