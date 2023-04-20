cc125efa8c3841114f55b01c0b72200f0767cfaf
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-04-20 18:58:56 +0200

    Update testnetLand.sol

--- contracts/NFT/testnetLand.sol
+++ contracts/NFT/testnetLand.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract DecentralizeXland is ERC721 {
     uint256 public tokenCount;
