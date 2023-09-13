c49d5cc699fadb64de944984b105a47114475a83
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-09-14 00:23:49 +0200

    Update distributionWithPenalties.sol

--- contracts/token-distribution/distributionWithPenalties.sol
+++ contracts/token-distribution/distributionWithPenalties.sol
@@ -9,20 +9,18 @@ import "../interface/IacPool.sol";
 import "../interface/IGovernor.sol";
 import "../interface/IVoting.sol";
 
-// more simple than a merkle-tree airdrop
+// merkle-tree airdrop
 // Distribution with penalties (for referrals, locked(illiquid) contributions, rewards(giveaways))
 contract AirDrop is ReentrancyGuard {
+	bytes32 public immutable merkleRoot =; //root
 	uint256 public constant CLAIM_PERIOD_DAYS = 90;
 
 	IDTX public immutable DTX;
 	IMasterChef public masterchef;
-	address public immutable initiatingAddress; // inititates balances
 
     uint256 public startTime;
     uint256 public directPayout = 250; // 97.5% penalty
-	uint256 public totalCredit;
-	uint256 public usersCredited; //makes it easy to verify
-	bool public creditGiven = false;
+	uint256 public totalRedeemed;
 
     address public acPool1;
     address public acPool2;
@@ -33,34 +31,35 @@ contract AirDrop is ReentrancyGuard {
 	
 	address public votingCreditContract;
 
-	mapping(address => uint256) public userCredit;
+	mapping(address => uint256) public amountRedeemed;
 	mapping(address => uint256) public minToServe;
     mapping(address => uint256) public payout;
 
-	event AddCredit(uint256 credit, address user);
 	event RedeemCredit(uint256 amount, address user, address withdrawInto);
 
 	constructor(IDTX _dtx, IMasterChef _chef) {
 		DTX = _dtx;
-		initiatingAddress = msg.sender;
 		startTime = block.timestamp;
 		masterchef = _chef;
 	}
 
-	function claimAirdrop(uint256 amount, address claimInto) external nonReentrant {
-		require(amount <= userCredit[msg.sender], "insufficient credit");
+	function claimAirdrop(uint256 _claimAmount, uint256 amount, address claimInto, bytes32[] calldata merkleProof) external nonReentrant {
+		require(isValid(msg.sender, amount, merkleProof), "Merkle proof invalid");
+		require(_claimAmount + amountRedeemed[msg.sender] <= amount, "insufficient credit");
+
 		if(claimInto == acPool1 || claimInto == acPool2 || claimInto == acPool3 || claimInto == acPool4 || claimInto == acPool5 || claimInto == acPool6) {
-			masterchef.publishTokens(address(this), amount * payout[claimInto] / 10000);
-			IacPool(claimInto).giftDeposit((amount * payout[claimInto] / 10000), msg.sender, minToServe[claimInto]);
-			IVoting(votingCreditContract).airdropVotingCredit(amount * payout[claimInto] / 10000, msg.sender);
+			masterchef.publishTokens(address(this), _claimAmount * payout[claimInto] / 10000);
+			IacPool(claimInto).giftDeposit((_claimAmount * payout[claimInto] / 10000), msg.sender, minToServe[claimInto]);
+			IVoting(votingCreditContract).airdropVotingCredit(_claimAmount * payout[claimInto] / 10000, msg.sender);
 		} else {
 			require(claimInto == msg.sender, "invalid recipient");
-			masterchef.publishTokens(msg.sender, (amount * directPayout / 10000));
+			masterchef.publishTokens(msg.sender, (_claimAmount * directPayout / 10000));
 		}
 
-		userCredit[msg.sender]-= amount;
+		amountRedeemed[msg.sender]+= _claimAmount;
+		totalRedeemed+= _claimAmount;
 
-		emit RedeemCredit(amount, msg.sender, claimInto);
+		emit RedeemCredit(_claimAmount, msg.sender, claimInto);
 	}
 
     // ends the airdrop by emptying token balance(sends tokens to governing contract)
@@ -68,6 +67,11 @@ contract AirDrop is ReentrancyGuard {
 		require(block.timestamp > startTime + CLAIM_PERIOD_DAYS * 86400, "airdrop still active");
 		masterchef.publishTokens(owner(), masterchef.credit(address(this))));
 	}
+
+	function isValid(address _user, uint256 amount, bytes32[] calldata merkleProof) public view returns(bool) {
+        bytes32 node = keccak256(abi.encodePacked(_user, amount));
+        return(MerkleProof.verify(merkleProof, merkleRoot, node));
+    }
 
 	function updatePools() external {
 			acPool1 = IGovernor(owner()).acPool1();
@@ -93,26 +97,6 @@ contract AirDrop is ReentrancyGuard {
 			payout[acPool5] = 5000;
 			payout[acPool6] = 10000;	
     }
-
-	function inititateBalances(uint256[] calldata amount, address[] calldata beneficiary) external {
-		require(msg.sender == initiatingAddress && !creditGiven, "only initiator allowed");
-		require(amount.length == beneficiary.length, "wrong list");
-		
-		for(uint256 i = 0; i < beneficiary.length; i++) {
-			require(userCredit[beneficiary[i]] = 0, "Already has balance");
-			usersCredited++;
-			userCredit[beneficiary[i]] = amount[i];
-			totalCredit+= amount[i];
-			
-			emit AddCredit(amount[i], beneficiary[i]);
-		}
-	} 
-
-	function endInitiation(address _creditContract) external {
-		require(msg.sender == initiatingAddress, "only initiator");
-		creditGiven = true;
-		votingCreditContract = _creditContract;
-	}
 
 	function owner() public view returns(address) {
 		return DTX.governor();
