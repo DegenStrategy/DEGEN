a6b0afb50e1d91156932f80bcdb93b4e2100ce9c
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-09-27 09:37:16 +0200

    Update 5year.sol

--- contracts/mining/5year.sol
+++ contracts/mining/5year.sol
@@ -806,7 +806,7 @@ contract TimeDeposit is ReentrancyGuard {
      * @return Expected reward to collect in DTX
      */
     function calculateHarvestDTXRewards() external view returns (uint256) {
-        uint256 amount = IMasterChef(masterchef).pendingDtx(poolID, address(this));
+        uint256 amount = IMasterChef(masterchef).pendingDtx(poolID);
         uint256 currentCallFee = amount.mul(callFee).div(10000);
 
         return currentCallFee;
@@ -816,7 +816,7 @@ contract TimeDeposit is ReentrancyGuard {
      * @return Returns total pending dtx rewards
      */
     function calculateTotalPendingDTXRewards() external view returns (uint256) {
-        uint256 amount = IMasterChef(masterchef).pendingDtx(poolID, address(this));
+        uint256 amount = IMasterChef(masterchef).pendingDtx(poolID);
 
         return amount;
     }
@@ -856,7 +856,7 @@ contract TimeDeposit is ReentrancyGuard {
      * @dev It includes tokens held by the contract and held in MasterChef
      */
     function balanceOf() public view returns (uint256) {
-        uint256 amount = IMasterChef(masterchef).pendingDtx(poolID, address(this)); 
+        uint256 amount = IMasterChef(masterchef).pendingDtx(poolID); 
 		uint256 _credit = IMasterChef(masterchef).credit(address(this));
         return amount.add(_credit); 
     }
