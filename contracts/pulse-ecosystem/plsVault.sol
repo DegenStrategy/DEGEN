72567de872f3f6ffab255a2b6a5040a832f548e1
decentralizeX <110387397+decentralizeX@users.noreply.github.com>
2023-10-19 01:10:31 +0200

    Update plsVault.sol

--- contracts/pulse-ecosystem/plsVault.sol
+++ contracts/pulse-ecosystem/plsVault.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.20;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/security/ReentrancyGuard.sol";
@@ -11,12 +10,11 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contr
 import "../interface/IGovernor.sol";
 import "../interface/IMasterChef.sol";
 import "../interface/IacPool.sol";
-import "../interface/IVoting.sol";
 
 /**
  * PLS vault
  */
-contract pulseVault is ReentrancyGuard {
+contract plsVault is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     struct UserInfo {
@@ -34,7 +32,7 @@ contract pulseVault is ReentrancyGuard {
 	uint256 public constant maxFee = 250; // max 2.5%
 	uint256 public constant maxFundingFee = 250; // max 0.025% per hour
 	
-    IERC20 public immutable token; // token
+    IERC20 public immutable token; //  token
 
     IMasterChef public masterchef;  
 
@@ -44,37 +42,39 @@ contract pulseVault is ReentrancyGuard {
 	// Referral system: Track referrer + referral points
 	mapping(address => address) public referredBy;
 	mapping(address => uint256) public referralPoints;
- 
+
 	uint256 public poolID; 
 	uint256 public accDtxPerShare;
     address public treasury; // Governing contract
 	address public treasuryWallet; // Actual treasury wallet
 
 	uint256 public lastCredit; // Keep track of our latest credit score from masterchef
-
+	
     uint256 public defaultDirectPayout = 50; //0.5% if withdrawn into wallet
 	
-	uint256 public depositFee = 0; // 0 
+	uint256 public depositFee = 0; // 0
 	uint256 public fundingRate = 0;// 0
 	uint256 public lastFundingChangeTimestamp; // save block.timestamp when funding rate is changed
 	
 
-    event Deposit(address indexed sender, uint256 amount, uint256 debt, uint256 depositFee, address referral);
+    event Deposit(address indexed sender, uint256 amount, uint256 debt, uint256 depositFee, address indexed referral);
     event Withdraw(address indexed sender, uint256 stakeID, uint256 harvestAmount, uint256 penalty);
-    event UserSettingUpdate(address indexed user, address poolAddress, uint256 threshold, uint256 feeToPay);
 
-    event SelfHarvest(address indexed user, address harvestInto, uint256 harvestAmount, uint256 penalty);
+    event SelfHarvest(address indexed user, address indexed harvestInto, uint256 harvestAmount, uint256 penalty);
 	
-	event CollectedFee(address from, uint256 amount);
+	event CollectedFee(address indexed from, uint256 amount);
 
+    /**
+     * @notice Constructor
+     * @param _masterchef: MasterChef contract
+     */
     constructor(
         IMasterChef _masterchef,
-		IERC20 _token
+	IERC20 _token
     ) {
         masterchef = _masterchef;
-		token = _token;
-		poolID = 6;
-		
+        poolID = 6;
+	token = _token;
 
 		poolPayout[].amount = 100;
         poolPayout[].minServe = 864000;
@@ -102,31 +102,30 @@ contract pulseVault is ReentrancyGuard {
         require(msg.sender == IMasterChef(masterchef).owner(), "Decentralized Voting Only!");
         _;
     }
-	
-    receive() external payable {}
-    fallback() external payable {}
 
+	receive() external payable {}
+    fallback() external payable {}
 	
     /**
      * Creates a NEW stake
-     * _poolInto is the pool to harvest into(time deposit option)
-	 * threshold is the amount to allow another user to harvest 
-	 * fee is the amount paid to harvester
      */
     function deposit(uint256 _amount, address referral) external payable nonReentrant {
         require(msg.value == _amount && _amount > 0, "invalid amount");
         harvest();
-		
+
 		if(referredBy[msg.sender] == address(0) && referral != msg.sender) {
 			referredBy[msg.sender] = referral;
 		}
-		
-		uint256 _depositFee = _amount * depositFee / 10000;
-		_amount = _amount - _depositFee;
 
-		payable(treasuryWallet).transfer(_depositFee );
+		uint256 _depositFee = 0;
+		if(depositFee != 0) {
+			_depositFee = _amount * depositFee / 10000;
+			_amount = _amount - _depositFee;
+
+			payable(treasuryWallet).transfer(_depositFee);
+		}
 		
-		uint256 _debt = _amount * accDtxPerShare / 1e12;
+		uint256 _debt = accDtxPerShare;
 
         userInfo[msg.sender].push(
                 UserInfo(_amount, _debt, _depositFee, block.timestamp)
@@ -139,9 +138,9 @@ contract pulseVault is ReentrancyGuard {
      * Harvests into pool
      */
     function harvest() public {
-		IMasterChef(masterchef).updatePool(poolID);
+        IMasterChef(masterchef).updatePool(poolID);
 		uint256 _currentCredit = IMasterChef(masterchef).credit(address(this));
-		uint256 _accumulatedRewards = lastCredit - _currentCredit;
+		uint256 _accumulatedRewards = _currentCredit - lastCredit;
 		lastCredit = _currentCredit;
 		accDtxPerShare+= _accumulatedRewards * 1e12  / address(this).balance;
     }
@@ -154,12 +153,12 @@ contract pulseVault is ReentrancyGuard {
         harvest();
         require(_stakeID < userInfo[msg.sender].length, "invalid stake ID");
         UserInfo storage user = userInfo[msg.sender][_stakeID];
-        
-        payFee(user, msg.sender);
+
+		uint256 currentAmount = user.amount * (accDtxPerShare - user.debt) / 1e12;
+		
+		payFee(user, msg.sender);
 
 		uint256 userTokens = user.amount; 
-
-		uint256 currentAmount = userTokens * accDtxPerShare / 1e12 - user.debt;
 		
 		_removeStake(msg.sender, _stakeID);
 
@@ -168,22 +167,24 @@ contract pulseVault is ReentrancyGuard {
         if(_harvestInto == msg.sender) { 
             _toWithdraw = currentAmount * defaultDirectPayout / 10000;
             currentAmount = currentAmount - _toWithdraw;
-			IMasterChef(masterchef).publishTokens(msg.sender, _toWithdraw);
-         } else {
+            IMasterChef(masterchef).publishTokens(msg.sender, _toWithdraw);
+        } else {
             require(poolPayout[_harvestInto].amount != 0, "incorrect pool!");
             _toWithdraw = currentAmount * poolPayout[_harvestInto].amount / 10000;
             currentAmount = currentAmount - _toWithdraw;
 			IMasterChef(masterchef).publishTokens(address(this), _toWithdraw);
             IacPool(_harvestInto).giftDeposit(_toWithdraw, msg.sender, poolPayout[_harvestInto].minServe);
         }
-		
+
 		if(referredBy[msg.sender] != address(0)) {
 			referralPoints[msg.sender]+= _toWithdraw;
 			referralPoints[referredBy[msg.sender]]+= _toWithdraw;
 		}
-		
-        IMasterChef(masterchef).publishTokens(treasury, currentAmount); //penalty goes to governing contract
 
+		if(currentAmount > 0) {
+        	IMasterChef(masterchef).publishTokens(treasury, currentAmount); //penalty goes to governing contract
+		}
+		
 		lastCredit = lastCredit - (_toWithdraw + currentAmount);
 		
 		emit Withdraw(msg.sender, _stakeID, _toWithdraw, currentAmount);
@@ -192,17 +193,17 @@ contract pulseVault is ReentrancyGuard {
     } 
 
 
-	function selfHarvest(uint256[] calldata _stakeID, address _harvestInto) external {
+	function selfHarvest(uint256[] calldata _stakeID, address _harvestInto) external nonReentrant {
         require(_stakeID.length <= userInfo[msg.sender].length, "incorrect Stake list");
         UserInfo[] storage user = userInfo[msg.sender];
         harvest();
         uint256 _toWithdraw = 0;
         uint256 _payout = 0;
- 
-        for(uint256 i = 0; i<_stakeID.length; i++) {
+
+        for(uint256 i = 0; i<_stakeID.length; ++i) {
+            _toWithdraw+= user[_stakeID[i]].amount * (accDtxPerShare - user[_stakeID[i]].debt)/ 1e12;
 			payFee(user[_stakeID[i]], msg.sender);
-            _toWithdraw+= user[_stakeID[i]].amount * accDtxPerShare / 1e12 - user[_stakeID[i]].debt;
-			user[_stakeID[i]].debt = user[_stakeID[i]].amount * accDtxPerShare / 1e12;
+			user[_stakeID[i]].debt = accDtxPerShare;
         }
 
         if(_harvestInto == msg.sender) {
@@ -214,7 +215,7 @@ contract pulseVault is ReentrancyGuard {
 			IMasterChef(masterchef).publishTokens(address(this), _payout);
             IacPool(_harvestInto).giftDeposit(_payout, msg.sender, poolPayout[_harvestInto].minServe);
 		}
-		
+
 		if(referredBy[msg.sender] != address(0)) {
 			referralPoints[msg.sender]+= _payout;
 			referralPoints[referredBy[msg.sender]]+= _payout;
@@ -222,15 +223,14 @@ contract pulseVault is ReentrancyGuard {
 
         uint256 _penalty = _toWithdraw - _payout;
 		IMasterChef(masterchef).publishTokens(treasury, _penalty); //penalty to treasury
-
+		
 		lastCredit = lastCredit - (_payout + _penalty);
 
 		emit SelfHarvest(msg.sender, _harvestInto, _payout, _penalty);        
     }
 
-
 	// emergency withdraw, without caring about rewards
-	function emergencyWithdraw(uint256 _stakeID) public {
+	function emergencyWithdraw(uint256 _stakeID) public nonReentrant {
 		require(_stakeID < userInfo[msg.sender].length, "invalid stake ID");
 		UserInfo storage user = userInfo[msg.sender][_stakeID];
 
@@ -252,8 +252,8 @@ contract pulseVault is ReentrancyGuard {
 	}
 	
 	function collectCommission(address[] calldata _beneficiary, uint256[][] calldata _stakeID) external nonReentrant {
-		for(uint256 i = 0; i< _beneficiary.length; i++) {
-			for(uint256 j = 0; j< _stakeID[i].length; i++) {
+		for(uint256 i = 0; i< _beneficiary.length; ++i) {
+			for(uint256 j = 0; j< _stakeID[i].length; ++j) {
                 UserInfo storage user = userInfo[_beneficiary[i]][_stakeID[i][j]];
                 payFee(user, _beneficiary[i]);
             }
@@ -261,11 +261,11 @@ contract pulseVault is ReentrancyGuard {
 	}
 	
 	function collectCommissionAuto(address[] calldata _beneficiary) external nonReentrant {
-		for(uint256 i = 0; i< _beneficiary.length; i++) {
+		for(uint256 i = 0; i< _beneficiary.length; ++i) {
 			
 			uint256 _nrOfStakes = getNrOfStakes(_beneficiary[i]);
 			
-			for(uint256 j = 0; j < _nrOfStakes; j++) {
+			for(uint256 j = 0; j < _nrOfStakes; ++j) {
                 UserInfo storage user = userInfo[_beneficiary[i]][j];
                 payFee(user, _beneficiary[i]);
             }
@@ -289,9 +289,9 @@ contract pulseVault is ReentrancyGuard {
 	}
 
 	function updateTreasury() external {
-        treasury = IMasterChef(masterchef).feeAddress();
+		treasury = IMasterChef(masterchef).feeAddress();
 		treasuryWallet = IGovernor(IMasterChef(masterchef).owner()).treasuryWallet();
-    }
+	}
 	
 	
 	/**
@@ -300,8 +300,9 @@ contract pulseVault is ReentrancyGuard {
 	 */
 	function withdrawStuckTokens(address _tokenAddress) external {
 		require(_tokenAddress != address(token), "illegal token");
+        require(_tokenAddress != address(stakeToken), "illegal token");
 		
-		IERC20(_tokenAddress).safeTransfer(IGovernor(IMasterChef(masterchef).owner()).treasuryWallet(), IERC20(_tokenAddress).balanceOf(address(this)));
+		IERC20(_tokenAddress).safeTransfer(treasuryWallet, IERC20(_tokenAddress).balanceOf(address(this)));
 	}
 
 	/*
@@ -313,7 +314,7 @@ contract pulseVault is ReentrancyGuard {
 		masterchef = _masterchef;
 		poolID = _newPoolID; //in case pool ID changes
 	}
-    
+
     //need to set pools before launch or perhaps during contract launch
     //determines the payout depending on the pool. could set a governance process for it(determining amounts for pools)
 	//allocation contract contains the decentralized proccess for updating setting, but so does the admin(governor)
@@ -322,14 +323,16 @@ contract pulseVault is ReentrancyGuard {
 		poolPayout[_poolAddress].amount = _amount;
 		poolPayout[_poolAddress].minServe = _minServe; //mandatory lockup(else stake for 5yr, withdraw with 82% penalty and receive 18%)
     }
+
     
     function updateSettings(uint256 _defaultDirectHarvest) external decentralizedVoting {
+		require(_defaultDirectHarvest <= 10_000, "maximum 100%");
         defaultDirectPayout = _defaultDirectHarvest;
     }
-
+    
     function viewStakeEarnings(address _user, uint256 _stakeID) external view returns (uint256) {
 		UserInfo storage _stake = userInfo[_user][_stakeID];
-        uint256 _pending = _stake.amount * virtualAccDtxPerShare() / 1e12 - _stake.debt;
+        uint256 _pending = _stake.amount * (virtualAccDtxPerShare() - _stake.debt) / 1e12 ;
         return _pending;
     }
 
@@ -339,8 +342,8 @@ contract pulseVault is ReentrancyGuard {
 
 		uint256 _totalPending = 0;
 		
-		for(uint256 i=0; i < nrOfUserStakes; i++) {
-			_totalPending+= _stake[i].amount * virtualAccDtxPerShare() / 1e12 - _stake[i].debt;
+		for(uint256 i=0; i < nrOfUserStakes; ++i) {
+			_totalPending+= _stake[i].amount * (virtualAccDtxPerShare() - _stake[i].debt) / 1e12 ;
 		}
 		
 		return _totalPending;
@@ -348,7 +351,7 @@ contract pulseVault is ReentrancyGuard {
 	//we want user deposit, we want total deposited, we want pending rewards, 
 	function multiCall(address _user, uint256 _stakeID) external view returns(uint256, uint256, uint256, uint256) {
 		UserInfo storage user = userInfo[_user][_stakeID];
-		uint256 _pending = user.amount * virtualAccDtxPerShare() / 1e12 - user.debt;
+		uint256 _pending = user.amount * (virtualAccDtxPerShare() - user.debt) / 1e12 ;
 		return(user.amount, user.feesPaid, address(this).balance, _pending);
 	}
 
@@ -362,7 +365,7 @@ contract pulseVault is ReentrancyGuard {
 	function viewPoolPayout(address _contract) external view returns (uint256) {
 		return poolPayout[_contract].amount;
 	}
-	
+
 	function viewPoolMinServe(address _contract) external view returns (uint256) {
 		return poolPayout[_contract].minServe;
 	}
@@ -377,8 +380,8 @@ contract pulseVault is ReentrancyGuard {
 	//public lookup for UI
     function publicBalanceOf() public view returns (uint256) {
         uint256 amount = IMasterChef(masterchef).pendingDtx(poolID); 
-		uint256 _credit = IMasterChef(masterchef).credit(address(this));
-        return _credit + amount; 
+        uint256 _credit = IMasterChef(masterchef).credit(address(this));
+        return _credit + amount;
     }
 
 	// With "Virtual harvest" for external calls
@@ -399,11 +402,15 @@ contract pulseVault is ReentrancyGuard {
 
         uint256 secondsSinceLastaction = block.timestamp - _lastAction;
 				
-		if(secondsSinceLastaction >= 3600 && fundingRate > 0) {
+		if(secondsSinceLastaction >= 3600  && fundingRate > 0) {
 			user.lastAction = block.timestamp - (secondsSinceLastaction % 3600);
 			
-			uint256 commission = (block.timestamp - _lastAction) / 3600 * user.amount * fundingRate / 100000;
-			
+			uint256 commission = (block.timestamp - _lastAction) / 3600 * user.amount * fundingRate / 1000000;
+
+			if(commission > user.amount * 2 / 10) {
+				commission = user.amount * 2 / 10;
+			}
+
 			payable(treasuryWallet).transfer(commission);
 
             user.feesPaid = user.feesPaid + commission;
