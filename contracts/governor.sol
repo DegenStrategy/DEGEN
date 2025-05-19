e4e666f4bd5ccbd3952b717a70f94e00e9293bb8
DegenStrategy <denisordel2@gmail.com>
2025-05-19 11:59:59 +0200

    Update governor.sol

--- contracts/governor.sol
+++ contracts/governor.sol
@@ -53,9 +53,6 @@ contract DTXgovernor {
 	uint256 public depositFee = 0;
 	uint256 public fundingRate = 200;
 
-	uint256 public mintingPhaseLaunchDate = 1738574301; //arbitrarily set
-	uint256 public lastTotalCredit; // Keeps track of last total credit from chef (sends 2.5% to reward contract)
-    
     uint256 public costToVote = 1000 * 1e18;  // 1000 coins. All proposals are valid unless rejected. This is a minimum to prevent spam
     uint256 public delayBeforeEnforce = 1 days; //minimum number of TIME between when proposal is initiated and executed
     
