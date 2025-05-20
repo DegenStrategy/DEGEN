a94a208d3a5295833fdaaf53122a25bc4d335e42
DegenStrategy <denisordel2@gmail.com>
2025-05-20 11:57:42 +0200

    Update 1year.sol

--- contracts/mining/1year.sol
+++ contracts/mining/1year.sol
@@ -129,17 +129,10 @@ contract TimeDeposit is ReentrancyGuard {
 	
 	event SetDelegate(address userDelegating, address delegatee);
 
-    /**
-     * @notice Constructor
-     * @param _token: DTX token contract
-     * @param _masterchef: MasterChef contract
-     */
-    constructor(
-        IDTX _token,
-        IMasterChef _masterchef
+   constructor(
     ) {
-        token = _token;
-        masterchef = _masterchef;
+        token = IDTX();
+        masterchef = IMasterChef();
         admin = msg.sender;
         poolID = 2;
     }
