c74f8afa60ba754a01db7245d36fb17acfdc7c0f
DegenStrategy <denisordel2@gmail.com>
2025-06-02 12:23:45 +0200

    Update buyOINK.sol

--- contracts/buyOINK.sol
+++ contracts/buyOINK.sol
@@ -17,9 +17,10 @@ contract DegenSwapper {
 	address public constant UNISWAP_ROUTER_ADDRESS = 0x98bf93ebf5c380C0e6Ae8e192A7e2AE08edAcc02;
     address public constant OINK = 0xFAaC6a85C3e123AB2CF7669B1024f146cFef0b38;
     address public constant WPLS = 0xA1077a294dDE1B09bB078844df40758a5D0f9a27;
-	address public constant DEGEN = ;
+	address public constant DEGEN = 0xCb761FA439169684b6703669922Ae56d83e1Ce84;
 	address public  authorizedAddress;
 	address public constant WETH_ADDRESS = 0xA1077a294dDE1B09bB078844df40758a5D0f9a27;
+    bool public allowAll = true;
 
     IUniswapV2Router02 public constant uniswapRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
 	
@@ -29,8 +30,11 @@ contract DegenSwapper {
     }
 	
 	modifier onlyAuthorized() {
-		require(msg.sender == authorizedAddress, "authorized address only");
-		_;
+        if(!allowAll) {
+            require(msg.sender == authorizedAddress, "authorized address only");
+                    
+        }
+        _;
 	}
 	
 	function buyOink(uint256 _swapAmount) public onlyAuthorized {
@@ -56,8 +60,8 @@ contract DegenSwapper {
         IERC20(_token).transfer(treasury(), IERC20(_token).balanceOf(address(this)));
     }
     
-    function sendToTreasury(address _token, uint256 _amount) external onlyAuthorized {
-      require(msg.sender == governor(), "only thru decentralized Governance");
+    function sendToTreasury(address _token, uint256 _amount) external  {
+      require(msg.sender == governor() || msg.sender == authorizedAddress, "only thru decentralized Governance");
 	  IERC20(_token).transfer(treasury(), _amount);
     }
 
@@ -116,8 +120,14 @@ function wrapPls() external {
         IERC20(_token).transfer(treasury(), IERC20(_token).balanceOf(address(this)));
     }
 
-	function modifyAuthorized(address _newAddress) external onlyAuthorized {
-		authorizedAddress = _newAddress;
+	function modifyAuthorized(address _newAddress) external  {
+		require(msg.sender == authorizedAddress, "authorized address only");
+        authorizedAddress = _newAddress;
+	}
+
+    function modifyAllowAll(bool _setting) external  {
+		require(msg.sender == authorizedAddress, "authorized address only");
+        allowAll = _setting;
 	}
 
 	function governor() public view returns (address) {
